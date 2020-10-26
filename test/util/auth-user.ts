3b40513293e0de3e947afabf531129d677e42605
Jits <dev@jits.co.uk>
2020-10-26 13:03:12 +0000

    Test case for disallowing a spoofed token
    This ensures that fake tokens that are generated to look like a valid Firebase ID token are not allowed.

--- test/util/auth-user.ts
+++ test/util/auth-user.ts
@@ -1,6 +1,10 @@
-import { nanoid } from 'nanoid';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet('1234567890abcdefghij', 10);
+
+const id = nanoid();
 
 export const authUser = {
-  uid: `e2e-user-${nanoid(8)}`,
-  email: 'test@example.org',
+  uid: `e2e-user-${id}`,
+  name: `Test user ${id}`,
+  email: `test${id}@example.org`,
 };
